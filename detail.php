be99da6ed080db3e52cfc29c2189aabc533fb254
Anika Henke <anika@selfthinker.org>
2014-05-19 00:18:39 +0100

    moved logic out of detail.php, using new core functions

--- detail.php
+++ detail.php
@@ -33,66 +33,30 @@ header('X-UA-Compatible: IE=edge,chrome=1');
     <div id="dokuwiki__detail" class="<?php echo tpl_classes(); ?>">
         <?php html_msgarea() ?>
 
-        <?php if($ERROR){ print $ERROR; }else{ ?>
+        <?php if($ERROR): print $ERROR; ?>
+        <?php else: ?>
 
-            <h1><?php echo hsc(tpl_img_getTag('IPTC.Headline',$IMG))?></h1>
+            <h1><?php echo hsc(tpl_img_getTag('IPTC.Headline', $IMG))?></h1>
 
             <div class="content">
-                <?php tpl_img(900,700); /* parameters: maximum width, maximum height (and more) */ ?>
+                <?php tpl_img(900, 700); /* the image; parameters: maximum width, maximum height (and more) */ ?>
 
                 <div class="img_detail">
                     <h2><?php print nl2br(hsc(tpl_img_getTag('simple.title'))); ?></h2>
 
-                    <dl>
-                        <?php
-                            $config_files = getConfigFiles('mediameta');
-                            foreach ($config_files as $config_file) {
-                                if(@file_exists($config_file)) {
-                                    include($config_file);
-                                }
-                            }
-
-                            foreach($fields as $key => $tag){
-                                $t = array();
-                                if (!empty($tag[0])) {
-                                    $t = array($tag[0]);
-                                }
-                                if(is_array($tag[3])) {
-                                    $t = array_merge($t,$tag[3]);
-                                }
-                                $value = tpl_img_getTag($t);
-                                if ($value) {
-                                    echo '<dt>'.$lang[$tag[1]].':</dt><dd>';
-                                    if ($tag[2] == 'date') {
-                                        echo dformat($value);
-                                    } else {
-                                        echo hsc($value);
-                                    }
-                                    echo '</dd>';
-                                }
-                            }
-                        ?>
-                    </dl>
-                    <?php //Comment in for Debug// dbg(tpl_img_getTag('Simple.Raw'));?>
+                    <?php tpl_img_meta(); ?>
+                    <?php //Comment in for Debug// dbg(tpl_img_getTag('Simple.Raw')); ?>
                 </div>
                 <div class="clearer"></div>
             </div><!-- /.content -->
 
             <p class="back">
-                <?php
-                    $imgNS = getNS($IMG);
-                    $authNS = auth_quickaclcheck("$imgNS:*");
-                    if (($authNS >= AUTH_UPLOAD) && function_exists('media_managerURL')) {
-                        $mmURL = media_managerURL(array('ns' => $imgNS, 'image' => $IMG));
-                        echo '<a href="'.$mmURL.'">'.$lang['img_manager'].'</a><br />';
-                    }
-                ?>
-                &larr; <?php echo $lang['img_backto']?> <?php tpl_pagelink($ID)?>
+                <?php tpl_action('mediaManager', 1) ?><br />
+                &larr; <?php tpl_action('img_backto', 1) ?>
             </p>
 
-        <?php } ?>
+        <?php endif; ?>
     </div>
     <!--[if ( lte IE 7 | IE 8 ) ]></div><![endif]-->
 </body>
 </html>
-
