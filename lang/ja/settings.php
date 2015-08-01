af1f7737048a34d672afcf91ecc6d962d8f82af2
Hideaki SAWADA <chuno@live.jp>
2015-08-02 04:23:44 +0900

    Create settings.php

---
+++ lang/ja/settings.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+*
+* @author Hideaki SAWADA <chuno@live.jp>
+*/
+$lang['discussionPage']   = 'コメントページ（空白の場合無効）';
+$lang['userPage']         = 'ユーザーページ（空白の場合無効）';
+$lang['hideTools']        = 'ログインしていない場合、ツールを隠す';
