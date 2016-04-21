bf8004a893a2cc8d50803838a19e3a606a98751b
Myeongjin <aranet100@gmail.com>
2016-04-21 15:08:26 +0900

    translation update

---
+++ lang/ko/settings.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Korean language file for config
+ *
+ */
+
+$lang['discussionPage']   = '토론 문서 (토론을 비활성화하려면 비워 두세요)';
+$lang['userPage']         = '사용자 문서 (사용자 문서를 비활성화하려면 비워 두세요)';
+$lang['hideTools']        = '로그인하지 않을 때 도구를 숨길까요?';
